13ba22af33b1b2a269e40a2a4ad52548b4278e87
chan <chan.kang@commure.com>
2024-11-07 15:58:23 -0500

    add dind

--- arc-runner-set.sh
+++ arc-runner-set.sh
@@ -3,6 +3,7 @@ NAMESPACE="arc-runners"
 helm install "${INSTALLATION_NAME}" \
     --namespace "${NAMESPACE}" \
     --create-namespace \
+    --set containerMode.type=dind \
     --set githubConfigUrl="${GITHUB_CONFIG_URL}" \
     --set githubConfigSecret.github_token="${GITHUB_PAT}" \
     oci://ghcr.io/actions/actions-runner-controller-charts/gha-runner-scale-set
