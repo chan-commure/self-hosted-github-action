824b4ea1f457a4b788d3c638b693acf6a4ba6b72
chan <chan.kang@commure.com>
2024-11-07 19:04:31 -0500

    one directory per chart

--- action-runner-controller/runner-set/arc-runner-set.sh
+++ action-runner-controller/runner-set/arc-runner-set.sh
@@ -3,7 +3,9 @@ NAMESPACE="arc-runners"
 helm install "${INSTALLATION_NAME}" \
     --namespace "${NAMESPACE}" \
     --create-namespace \
+    --set controllerServiceAccount.name="${INSTALLATION_NAME}" \
+    --set controllerServiceAccount.namespace="${INSTALLATION_NAME}" \
     --set containerMode.type=dind \
     --set githubConfigUrl="${GITHUB_CONFIG_URL}" \
     --set githubConfigSecret.github_token="${GITHUB_PAT}" \
-    oci://ghcr.io/actions/actions-runner-controller-charts/gha-runner-scale-set
+    oci://ghcr.io/actions/actions-runner-controller-charts/gha-runner-scale-set --dry-run
--- action-runner-controller/systems/arc-systems.sh
+++ action-runner-controller/systems/arc-systems.sh
@@ -2,4 +2,4 @@ NAMESPACE="arc-systems"
 helm install arc \
     --namespace "${NAMESPACE}" \
     --create-namespace \
-    oci://ghcr.io/actions/actions-runner-controller-charts/gha-runner-scale-set-controller
+    oci://ghcr.io/actions/actions-runner-controller-charts/gha-runner-scale-set-controller --dry-run
